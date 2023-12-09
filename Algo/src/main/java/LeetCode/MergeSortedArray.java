3c1a928e15eb585cdfbe5f6d7af8d311d9a462fe
Ramana <ramana.bvr143@gmail.com>
2023-12-09 20:14:51 +0530

    Tutor

---
+++ Algo/src/main/java/LeetCode/MergeSortedArray.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int[] nums2 = new int[]{2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
+
+}
